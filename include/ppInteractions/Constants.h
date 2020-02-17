923d6e3213805b6083356f240d5199bcaf626e8d
Rafa <8rafael@gmail.com>
2020-02-17 12:39:27 -0300

    masses and lifetimes in separate file

---
+++ include/ppInteractions/Constants.h
@@ -0,0 +1,16 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <crpropa/Units.h>
+
+// particle masses in eV
+const double mChargedPion = 1.39570 * 1e8 * crpropa::eV / crpropa::c_squared;
+const double mNeutralPion = 1.34977 * 1e8 * crpropa::eV / crpropa::c_squared;
+const double mEtaMeson = 5.47862 * 1e8 * crpropa::eV / crpropa::c_squared;
+const double mMuon = 1.0566 * 1e8 * crpropa::eV / crpropa::c_squared;
+const double tauChargedPion = 2.6033e-8;
+const double tauNeutralPion = 8.4e-17;
+const double tauEtaMeson = 5e-19; 
+const double tauMuon = 2.1969811e-6;
+
+#endif
