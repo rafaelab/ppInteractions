e7c74384db0a4d0284bf1328fb95ae045ae2acf6
Rafa <8rafael@gmail.com>
2020-03-10 10:14:51 +0100

    clean headers

--- include/ppInteractions/NucleusNucleusInteraction.h
+++ include/ppInteractions/NucleusNucleusInteraction.h
@@ -19,21 +19,13 @@
 #include <crpropa/Candidate.h>
 #include <crpropa/Vector3.h>
 
+#include "ppInteractions/Constants.h"
 #include "ppInteractions/ParticleDecay.h"
 
 #ifdef _OPENMP
 	#include "omp.h"
 #endif
 
-// particle masses in eV
-const double mChargedPion = 1.39570 * 1e8 * crpropa::eV / crpropa::c_squared;
-const double mNeutralPion = 1.34977 * 1e8 * crpropa::eV / crpropa::c_squared;
-const double mEtaMeson = 5.47862 * 1e8 * crpropa::eV / crpropa::c_squared;
-const double mMuon = 1.0566 * 1e8 * crpropa::eV / crpropa::c_squared;
-const double tauChargedPion = 2.6033e-8;
-const double tauNeutralPion = 8.4e-17;
-const double tauEtaMeson = 5e-19; 
-const double tauMuon = 2.1969811e-6;
 
 /**
  @class NucleusNucleusInteraction
