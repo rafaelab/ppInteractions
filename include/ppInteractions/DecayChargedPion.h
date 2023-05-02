39428aa958ba997ae124a395bd1e4e13870cc473
Rafael <rafaelab@users.noreply.github.com>
2023-05-02 13:33:16 -0400

    minor cosmetic improvements

--- include/ppInteractions/DecayChargedPion.h
+++ include/ppInteractions/DecayChargedPion.h
@@ -12,23 +12,22 @@
 #include "ppInteractions/Constants.h"
 
 
-class DecayChargedPion
-{
-public:
+class DecayChargedPion {
+	protected:
+		bool haveMuons;
+		bool haveNeutrinos;
+		double limit;
+		double thinning;
 
-	bool haveMuons;
-	bool haveNeutrinos;
-	double limit;
-	double thinning;
-
-	DecayChargedPion(bool muons = true, bool neutrinos = true, double thinning = 0, double limit = 0.05);
-	void setLimit(double limit);
-	void setThinning(double thinning);
-	void setHaveMuons(bool muons);
-	void setHaveNeutrinos(bool neutrinos);
-	double lossLength(double lorentzFactor) const; 
-	double energyFractionMuon() const; 
-	void performInteraction(crpropa::Candidate *candidate) const;
+	public:
+		DecayChargedPion(bool muons = true, bool neutrinos = true, double thinning = 0, double limit = 0.05);
+		void setLimit(double limit);
+		void setThinning(double thinning);
+		void setHaveMuons(bool muons);
+		void setHaveNeutrinos(bool neutrinos);
+		double lossLength(const double& lorentzFactor) const; 
+		double energyFractionMuon() const; 
+		void performInteraction(crpropa::Candidate* candidate) const;
 };
 
 
--- src/DecayChargedPion.cc
+++ src/DecayChargedPion.cc
@@ -2,6 +2,7 @@
 
 using namespace crpropa;
 
+
 DecayChargedPion::DecayChargedPion(bool muons, bool neutrinos, double thinning, double limit) {
 	setHaveMuons(muons);
 	setHaveNeutrinos(neutrinos);
@@ -25,21 +26,17 @@ void DecayChargedPion::setThinning(double thinning) {
 	thinning = thinning;
 }
 
-double DecayChargedPion::lossLength(double lf) const {
+double DecayChargedPion::lossLength(const double& lf) const {
 	// Returns the loss length in the lab frame.
-	double lifetime;
-	lifetime = tauChargedPion;
+	const static double lifetime = tauChargedPion;
 	return c_light * lifetime * lf;
 }
 
 double DecayChargedPion::energyFractionMuon() const {
-	// Random &random = Random::instance();
-	// double r = 1 - pow(mMuon / mChargedPion, 2);
-	// return random.randUniform(0., 1 - r);
-	return pow(mMuon / mChargedPion, 2.);
+	return pow_integer<2>(mMuon / mChargedPion);
 }
 
-void DecayChargedPion::performInteraction(Candidate *candidate) const {
+void DecayChargedPion::performInteraction(Candidate* candidate) const {
 
 	int id = candidate->current.getId();
 	double sign = (id > 0) ? 1 : ((id < 0) ? -1 : 0);
